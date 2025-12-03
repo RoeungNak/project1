d55309d080acc6d4ffb12ceb7cfed4cf039921fa
Roeung-Nak <rouengnak.biu@gmail.com>
2025-12-03 22:18:02 +0700

    main

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,7 @@ use App\Http\Middleware\CheckUser;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -18,6 +19,13 @@ return Application::configure(basePath: dirname(__DIR__))
             'checkAdminRole' => CheckAdmin::class,
             'checkUserRole' => CheckUser::class
         ]);
+        $middleware->trustProxies(
+            ['*'], // trust all proxies
+            Request::HEADER_X_FORWARDED_FOR |
+                Request::HEADER_X_FORWARDED_HOST |
+                Request::HEADER_X_FORWARDED_PORT |
+                Request::HEADER_X_FORWARDED_PROTO
+        );
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
